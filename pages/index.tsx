b88de1c21e98973aac42e2fdbed497c0927e1a7f
ksowah <sowahkelvin640@gmail.com>
2022-11-18 00:09:19 +0000

    structure display

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,10 +2,15 @@ import type { NextPage } from 'next'
 import Head from 'next/head'
 import Header from '../components/Header'
 import Hero from '../components/Hero'
+import SectionOne from '../components/SectionOne'
+import SectionTwo from '../components/SectionTwo'
+import SectionThree from '../components/SectionThree'
+import SectionFour from '../components/SectionFour'
+import Footer from '../components/Footer'
 
 const Home: NextPage = () => {
   return (
-    <div className="">
+    <div className="bg-white">
       <Head>
         <title>Create Next App</title>
         <link rel="icon" href="/favicon.ico" />
@@ -13,9 +18,15 @@ const Home: NextPage = () => {
 
       <Header />
       <Hero />
-      <main className="w-full xl:w-[1150px] mx-auto">
-
+      <main className="w-full xl:w-[1150px] mx-auto bg-white">
+        <SectionOne />
+        <SectionTwo />
+        <SectionThree />
       </main>
+
+      <SectionFour />
+
+      <Footer />
     </div>
   )
 }
