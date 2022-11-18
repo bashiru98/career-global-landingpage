1bea4423e23f0099719e77a9548c25c9d7940fca
ksowah <sowahkelvin640@gmail.com>
2022-11-18 13:00:27 +0000

    some style changes

--- components/Header.tsx
+++ components/Header.tsx
@@ -6,7 +6,7 @@ const Header = () => {
   return (
     <div className="w-full fixed h-[5rem] bg-[#0F072C] text-white z-50">
       <div className="relative flex items-center justify-between w-full xl:w-[1150px] mx-auto h-full">
-      <Image alt="" src={"	https://surge.global/wp-content/uploads/2022/07/surge-global-logo-in-white.svg"} width={100} height={50} />
+      <Image className="cursor-pointer" alt="" src={"	https://surge.global/wp-content/uploads/2022/07/surge-global-logo-in-white.svg"} width={100} height={50} />
 
     <div className="flex items-center space-x-10">
         <p className="cursor-pointer hover:text-gray-400 duration-700">Product</p>
