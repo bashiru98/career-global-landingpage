3ea84b7c8dfd83d9993eb713bf281e9cf459acdf
ksowah <sowahkelvin640@gmail.com>
2022-11-18 12:36:03 +0000

    add up button

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,12 +1,13 @@
-import type { NextPage } from 'next'
-import Head from 'next/head'
-import Header from '../components/Header'
-import Hero from '../components/Hero'
-import SectionOne from '../components/SectionOne'
-import SectionTwo from '../components/SectionTwo'
-import SectionThree from '../components/SectionThree'
-import SectionFour from '../components/SectionFour'
-import Footer from '../components/Footer'
+import type { NextPage } from "next";
+import Head from "next/head";
+import Header from "../components/Header";
+import Hero from "../components/Hero";
+import SectionOne from "../components/SectionOne";
+import SectionTwo from "../components/SectionTwo";
+import SectionThree from "../components/SectionThree";
+import SectionFour from "../components/SectionFour";
+import Footer from "../components/Footer";
+import { BsArrowUp } from "react-icons/bs";
 
 const Home: NextPage = () => {
   return (
@@ -15,6 +16,10 @@ const Home: NextPage = () => {
         <title>Create Next App</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
+
+      <div className="fixed right-20 bottom-20 cursor-pointer h-[3rem] w-[3rem] bg-[#0f072c] flex items-center justify-center text-white">
+        <BsArrowUp size={20} />
+      </div>
 
       <Header />
       <Hero />
@@ -28,7 +33,7 @@ const Home: NextPage = () => {
 
       <Footer />
     </div>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
